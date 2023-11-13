df15ddbc2a07b4dc4b57507f3e5f96a7b4a3c6f3
hryoshihara0692 <hr.yoshihara0692@gmail.com>
2023-11-13 16:23:32 +0900

    2023/11/13 ボタンをウィジェット化し、初期画面(仮)作成まで

--- lib/pages/initial.dart
+++ lib/pages/initial.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:todo/screen_pod.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:todo/components/ad_mob.dart';
+import 'package:todo/widgets/login_button.dart';
 
 class InitialPage extends StatefulWidget {
   // const initialPage({super.key});
@@ -14,7 +16,6 @@ class InitialPage extends StatefulWidget {
 
 class _InitialPageState extends State<InitialPage> {
   final AdMob _adMob = AdMob();
-  int _counter = 0;
 
   @override
   void initState() {
@@ -28,55 +29,68 @@ class _InitialPageState extends State<InitialPage> {
     _adMob.dispose();
   }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    final screen = ScreenRef(context).watch(screenProvider);
+    final designW = screen.designW(200);
+    final designH = screen.designH(200);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            FutureBuilder(
-                future: AdSize.getAnchoredAdaptiveBannerAdSize(
-                    Orientation.portrait,
-                    MediaQuery.of(context).size.width.truncate()),
-                builder: (BuildContext context,
-                    AsyncSnapshot<AnchoredAdaptiveBannerAdSize?> snapshot) {
-                  if (snapshot.hasData) {
-                    return SizedBox(
-                      width: double.infinity,
-                      child: _adMob.getAdBanner(),
-                    );
-                  } else {
-                    return Container(
-                      height: _adMob.getAdBannerHeight(),
-                      color: Colors.white,
-                    );
-                  }
-                }),
-          ],
+        body: SafeArea(
+      child: Column(children: [
+        Container(
+          width: designW,
+          height: designH,
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.red,
+          ),
+          margin: const EdgeInsets.fromLTRB(0, 100, 0, 100),
+          child: const Center(child: Text('Flex 1')),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        Expanded(
+          child: Container(
+            color: Colors.grey,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  RoundButton(
+                      buttonName: '新しく登録する',
+                      onPressed: () {
+                        print('test');
+                      }),
+                  const SizedBox(
+                    height: 50.0,
+                  ),
+                  RoundButton(
+                      buttonName: 'データを復元する',
+                      onPressed: () {
+                        print('test');
+                      }),
+                ])
+              ],
+            ),
+          ),
+        ),
+        FutureBuilder(
+            future: AdSize.getAnchoredAdaptiveBannerAdSize(Orientation.portrait,
+                MediaQuery.of(context).size.width.truncate()),
+            builder: (BuildContext context,
+                AsyncSnapshot<AnchoredAdaptiveBannerAdSize?> snapshot) {
+              if (snapshot.hasData) {
+                return SizedBox(
+                  width: double.infinity,
+                  child: _adMob.getAdBanner(),
+                );
+              } else {
+                return Container(
+                  height: _adMob.getAdBannerHeight(),
+                  color: Colors.white,
+                );
+              }
+            }),
+      ]),
+    ));
   }
 }
