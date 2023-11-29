53e2aac70ef1847697ad06b059869d31b8402807
hryoshihara0692 <hr.yoshihara0692@gmail.com>
2023-11-29 19:47:51 +0900

    2023/11/29 サイドメニュー内容を非ログイン時版として少し修正

--- lib/widgets/side_menu.dart
+++ lib/widgets/side_menu.dart
@@ -9,7 +9,7 @@ class SideMenu extends StatelessWidget {
       child: ListView(
         children: [
           SizedBox(
-            height: 80,
+            height: 120,
             child: DrawerHeader(
               decoration: BoxDecoration(),
               child: Container(
@@ -18,16 +18,32 @@ class SideMenu extends StatelessWidget {
             ),
           ),
           ListTile(
-            leading: Icon(Icons.person),
-            title: Text('Person1'),
+            leading: Icon(Icons.login),
+            title: Text('ログイン'),
+          ),
+          ListTile(
+            leading: Icon(Icons.person_add),
+            title: Text('新規登録'),
+          ),
+          ListTile(
+            leading: Icon(Icons.notifications),
+            title: Text('通知設定'),
           ),
           ListTile(
             leading: Icon(Icons.person),
-            title: Text('Person2'),
+            title: Text('プレミアムプラン'),
+          ),
+                    ListTile(
+            leading: Icon(Icons.person),
+            title: Text('ヘルプ'),
           ),
           ListTile(
             leading: Icon(Icons.person),
-            title: Text('Person3'),
+            title: Text('ご要望・不具合'),
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('アプリについて'),
           ),
         ],
       ),
