e96b9f8df90a515a48e1ff22da06268dbfa1598c
hryoshihara0692 <hr.yoshihara0692@gmail.com>
2023-12-01 13:19:10 +0900

    2023/12/01 サイドメニューの表示項目をログイン状況で変わるように修正

--- lib/widgets/side_menu.dart
+++ lib/widgets/side_menu.dart
@@ -1,59 +1,113 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SideMenu extends StatelessWidget {
   const SideMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        children: [
-          SizedBox(
-            height: 120,
-            child: DrawerHeader(
-              decoration: BoxDecoration(),
-              child: Container(
-                color: Colors.yellow,
+    final String uid = FirebaseAuth.instance.currentUser!.uid.toString();
+    if (uid == null) {
+      return Drawer(
+        child: ListView(
+          children: [
+            SizedBox(
+              height: 120,
+              child: DrawerHeader(
+                decoration: BoxDecoration(),
+                child: Container(
+                  color: Colors.yellow,
+                ),
               ),
             ),
-          ),
-          ListTile(
-            leading: Icon(Icons.login),
-            title: Text('ログイン'),
-            onTap: () {
-              context.push('/login');
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.person_add),
-            title: Text('新規登録'),
-            onTap: () {
-              context.push('/account_registration');
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.notifications),
-            title: Text('通知設定'),
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text('プレミアムプラン'),
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text('ヘルプ'),
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text('ご要望・不具合'),
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text('アプリについて'),
-          ),
-        ],
-      ),
-    );
+            ListTile(
+              leading: Icon(Icons.login),
+              title: Text('ログイン'),
+              onTap: () {
+                context.push('/login');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.person_add),
+              title: Text('新規登録'),
+              onTap: () {
+                context.push('/account_registration');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.notifications),
+              title: Text('通知設定'),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('プレミアムプラン'),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('ヘルプ'),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('ご要望・不具合'),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('アプリについて'),
+            ),
+          ],
+        ),
+      );
+    } else {
+      return Drawer(
+        child: ListView(
+          children: [
+            SizedBox(
+              height: 120,
+              child: DrawerHeader(
+                decoration: BoxDecoration(),
+                child: Container(
+                  color: Colors.yellow,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.login),
+              title: Text('ユーザIDあります'),
+              onTap: () {
+                context.push('/login');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.person_add),
+              title: Text('新規登録'),
+              onTap: () {
+                context.push('/account_registration');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.notifications),
+              title: Text('通知設定'),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('プレミアムプラン'),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('ヘルプ'),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('ご要望・不具合'),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('アプリについて'),
+            ),
+          ],
+        ),
+      );
+    }
   }
 }
