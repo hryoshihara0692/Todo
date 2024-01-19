773e176d4de3a243e841673f11eef4202fb12854
hryoshihara0692 <hr.yoshihara0692@gmail.com>
2024-01-20 01:50:02 +0900

    2023/01/20 ホーム画面起動に変更

--- lib/main.dart
+++ lib/main.dart
@@ -31,8 +31,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // home: const HomePage(title: 'Admob Demo'),
-      home: const InitialPage(title: 'Admob Demo'),
+      home: const HomePage(title: 'Admob Demo'),
+      // home: const InitialPage(title: 'Admob Demo'),
     );
   }
 }
