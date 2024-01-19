4e6df26db1e97957c93ed1c77a8389a949c201dd
hryoshihara0692 <hr.yoshihara0692@gmail.com>
2024-01-20 01:50:42 +0900

    2023/01/20 リスト表示、ボタンでタスクが追加される形式に変更

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -19,6 +19,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final AdMob _adMob = AdMob();
   int _counter = 0;
+  List<String> tasks = ["test", "てすと", "テスト"];
 
   @override
   void initState() {
@@ -35,6 +36,7 @@ class _HomePageState extends State<HomePage> {
   void _incrementCounter() {
     setState(() {
       _counter++;
+      tasks.add('ボタン押したよ$_counter');
     });
   }
 
@@ -44,35 +46,38 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Expanded(
+              child: ListView(
+                children: _createList(tasks),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            ElevatedButton(
+              onPressed: () => _incrementCounter(),
+              child: Text("a"),
             ),
+            // Admob
             FutureBuilder(
-                future: AdSize.getAnchoredAdaptiveBannerAdSize(
-                    Orientation.portrait,
-                    MediaQuery.of(context).size.width.truncate()),
-                builder: (BuildContext context,
-                    AsyncSnapshot<AnchoredAdaptiveBannerAdSize?> snapshot) {
-                  if (snapshot.hasData) {
-                    return SizedBox(
-                      width: double.infinity,
-                      child: _adMob.getAdBanner(),
-                    );
-                  } else {
-                    return Container(
-                      height: _adMob.getAdBannerHeight(),
-                      color: Colors.white,
-                    );
-                  }
-                }),
+              future: AdSize.getAnchoredAdaptiveBannerAdSize(
+                  Orientation.portrait,
+                  MediaQuery.of(context).size.width.truncate()),
+              builder: (BuildContext context,
+                  AsyncSnapshot<AnchoredAdaptiveBannerAdSize?> snapshot) {
+                if (snapshot.hasData) {
+                  return SizedBox(
+                    width: double.infinity,
+                    child: _adMob.getAdBanner(),
+                  );
+                } else {
+                  return Container(
+                    height: _adMob.getAdBannerHeight(),
+                    color: Colors.white,
+                  );
+                }
+              },
+            ),
           ],
         ),
       ),
@@ -83,4 +88,41 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+}
+
+List<ListTile> _createList(List<String> tasks) {
+  var _list = <ListTile>[];
+
+  for (var task in tasks) {
+    _list.add(
+      ListTile(
+        leading: Container(
+          width: 30,
+          height: 30,
+          decoration: const BoxDecoration(
+            shape: BoxShape.rectangle,
+            color: Colors.blue,
+          ),
+        ),
+        title: Text(task),
+        subtitle: Text(task),
+        trailing: Container(
+          width: 30,
+          height: 30,
+          margin: EdgeInsets.fromLTRB(5, 0, 5, 0),
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            color: Colors.green,
+          ),
+        ),
+        dense: true,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(30),
+          ),
+        ),
+      ),
+    );
+  }
+  return _list;
 }
